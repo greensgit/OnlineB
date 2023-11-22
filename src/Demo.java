895fcb6592f2a19bc6313a14dbc6b0db0e4f0ce7
greensgit <rajgreensomr@gmail.com>
2023-11-22 10:19:03 +0530

    new code

---
+++ src/Demo.java
@@ -0,0 +1,13 @@
+
+public class Demo {
+
+	private void demoOne() {
+		System.out.println("This is a test example");
+	}
+	
+	public static void main(String[] args) {
+		
+		Demo d = new Demo();
+		d.demoOne();
+	}
+}
